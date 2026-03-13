bed7db01a6b550a6128b2ea043df1058605c128a
Harsh Suryawanshi <harshsuryawanshi.0910@gmail.com>
2026-03-13 22:37:47 +0530

    test_mongodb.py updated

--- test_mongodb.py
+++ test_mongodb.py
@@ -1,7 +1,7 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-uri = "mongodb+srv://harshsuryawanshi:Admin123@cluster0.iji4agu.mongodb.net/?retryWrites=true&w=majority"
+uri = "mongodb+srv://harshsuryawanshi:harsh@cluster0.iji4agu.mongodb.net/?retryWrites=true&w=majority"
 
 client = MongoClient(uri, server_api=ServerApi('1'))
 
