0fd2392428ed9810b89c486d501c909147cf37dc
Harsh Suryawanshi <harshsuryawanshi.0910@gmail.com>
2026-03-20 22:13:44 +0530

    Data Ingestion Variable Name

--- networksecurity/constant/training_pipelie/__init__.py
+++ networksecurity/constant/training_pipelie/__init__.py
@@ -3,10 +3,21 @@ import sys
 import numpy as np
 import pandas as pd
 
+
+# Defining common constant variable for training pipeline
+
+TARGET_COLUMN = "Result"
+PIPLINE_NAME: str = "NetworkSecurity"
+ARTIFACT_DIR: str = "Artifacts"
+FILE_NAME: str = "phisingData.csv"
+
+TRAIN_FILE: str = "train.csv"
+TEST_FILE_NAME: str = "test.csv"
+
 # Data Ingestion related constant start with DATA_INGESTION VAR name
 
 DATA_INGESTION_COLLECTION_NAME: str = "NetworkData"
-DATA_INGESTION_DATABASE_NAME: str = "HSSolution"
+DATA_INGESTION_DATABASE_NAME: str = "KRISHAI"
 DATA_INGESTION_DIR_NAME: str = "data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_name"
 DATA_INGESTION_INGESTED_DIR: str = "ingested"
--- networksecurity/entity/config_entity.py
+++ networksecurity/entity/config_entity.py
@@ -1,3 +1,22 @@
 from datetime import datetime
 import os
 
+from networksecurity.constant import training_pipeline
+
+print(training_pipeline.PIPELINE_NAME)
+print(training_pipeline.ARTIFACT_DIR)
+
+
+class TrainingPipelienConfig:
+    def __init__(self,timestamp=datetime.now()):
+        timestamp=timestamp.strftime("%m_%d_%Y_%H_%M_%S")
+        self.pipeline_name=training_pipeline.PIPELINE_NAME
+        self.artifact_name=training_pipeline.ARTIFACT_DIR
+        self.artifact_dir=os.path.join(self.artifact_name,timestamp)
+        self.timestamp: str=timestamp
+
+class DataIngestionConfig:
+    def __init__(self,training_pipeline_config:TrainingPipelineConfig):
+        self.data_ingestion_dir:str = os.path.join(
+            training_pipeline_config.artifact_dir,training_pipeline.DATA_INGESTIONM_DIR_NAME
+        )
